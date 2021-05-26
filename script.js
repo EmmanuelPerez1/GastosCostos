182883e19d1f2a07364822dddb6e770f324717c4
EmmanuelPerez1 <arreolae475@gmail.com>
2021-05-26 07:27:17 -0500

    Esqueleto de script.js - Funciones por realizar

---
+++ script.js
@@ -0,0 +1,42 @@
+// Principapl objects
+var income = new Object(),
+    nameIncome = '',
+    cuantityIncome, dateIncome;
+
+var exit = new Object(),
+    nameExit = '',
+    cuantityExit, dateExit;
+
+// Object with arrays wich are going to store the objets of each past income and exit 
+var netIncome = new Object(),
+    logNetIncome = new Array,
+    logNegativeNetIncome = new Array;
+
+// Functions
+
+function createNewIncomes(nameIncome, cuantityIncome, dateIncome) {
+
+}
+
+function createNewExits(nameExit, cuantityExit, dateExit) {
+
+}
+
+function addNewIncomes(nameIncome, cuantityIncome, dateIncome) {
+
+}
+
+function addNewExits(nameExit, cuantityExit, dateExit) {
+
+}
+
+function logIncome(arrayLogIncome) {
+
+}
+
+function logIncome(arrayLogExits) {
+
+}
+
+// Todo: Print graph to give a better way to analize the net income
+// function printGrapg(arrayLogIncome, arrayLogExits) {}
