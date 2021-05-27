b5efa7d8efed1d3febdaac8fe859a8bae9e89efa
EmmanuelPerez1 <arreolae475@gmail.com>
2021-05-27 17:36:11 -0500

    Refactor funciones createNewExpane/Entries, función dateOfcreation ya muestra la fecha actual correctamente

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-// Principapl objects
-// var income = new Object(),
-//     nameIncome = '',
-//     cuantityIncome, dateIncome;
-// var exit = new Object(),
-//     nameExit = '',
-//     cuantityExit, dateExit;
-// // Object with arrays wich are going to store the objets of each past income and exit 
-// var netIncome = new Object(),
-//     logNetIncome = new Array,
-//     logNegativeNetIncome = new Array;
-
-// Functions
-
-function createNewIncomes(nameIncome, cuantityIncome, dateIncome) {
-    var income = {
-        NameIncome: nameIncome,
-        CuantityIncome: cuantityIncome,
-        DateIncome: dateIncome
+function createNewIncomes(...values) {
+    // if (nameIncome == '' || isNaN(cuantityIncome)) return false;
+    // return createObject(nameIncome, cuantityIncome, dateIncome);
+    for (const key in values) {
+        if (key == "" || isNaN(key)) {
+            return false
+        }
+        return createObject(values);
     }
-    return income;
 }
 
-function createNewExits(nameExit, cuantityExit, dateExit) {
+function createNewExpense(...values) {
+    // nameExpense, cuantityExpense, dateExpense
+    // if (nameIncome == '' || isNaN(cuantityIncome)) return false;
+    // var newExpanse = [nameExpense, cuantityExpense, dateExpense];
+    for (const key in values) {
+        if (key == "" || isNaN(key)) {
+            return false
+        }
+        return createObject(values);
+    }
+}
 
+function createObject(...value) {
+    var newObject = new Object();
+    for (const key of value) {
+        newObject[key] = key;
+    }
+    console.log(newObject);
+    return newObject;
 }
 
 function addNewIncomes(nameIncome, cuantityIncome, dateIncome) {
@@ -43,7 +48,7 @@ function logIncome(arrayLogExits) {
 
 function dateOfCreation() {
     var dOC = new Date();
-    var date = dOC.getFullYear() + '-' + dOC.getMonth() + '-' + dOC.getDay();
+    var date = dOC.getFullYear() + '-' + (dOC.getMonth() + 1) + '-' + dOC.getDate();
 
     return date;
 }
@@ -54,14 +59,30 @@ function dateOfCreation() {
 // Buttons functionality
 
 const btnNewExpenses = document.querySelector('#nameExpense+button');
+const btnNewIncomes = document.querySelector('#nameEntries+button');
 //Todo: fix the error wich shows every time the button #nameExpense+button shows an error in
 // console for not being click what it's press
+
 
 btnNewExpenses.addEventListener('click', (e) => {
     e.preventDefault();
     var inputCuantityExpense = document.querySelector('#cuantityExpense').value;
     var inputNameExpense = document.querySelector('#nameExpense').value;
     var dateNewExpense = dateOfCreation();
-    const nNI = createNewIncomes(inputNameExpense, inputCuantityExpense, dateNewExpense);
-    console.log(nNI);
+    const nNI = createNewExpense(inputNameExpense, inputCuantityExpense, dateNewExpense);
+    // console.log(nNI.value);
+    for (const key in nNI) {
+        console.log(key);
+    }
+});
+
+btnNewIncomes.addEventListener('click', (e) => {
+    e.preventDefault();
+    var inputCuantityEntrie = document.querySelector('#cuantityEntrie').value;
+    var inputNameEntrie = document.querySelector('#nameEntries').value;
+    var dateNewEntrie = dateOfCreation();
+    const nNE = createNewIncomes(inputNameEntrie, inputCuantityEntrie, dateNewEntrie);
+    for (const key in nNE) {
+        console.log(key);
+    }
 });
